08c0c8517121e093948faefe9cee9d49dba7f35b
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-05-18 11:04:32 -0700

    Daily Report for Tech Academy Students - commit

---
+++ DailyReport/DailyReport.cs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System; //library actively used by document
+using System.Collections.Generic; //this and all the libraries below it are grayed because they are not used
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport //makes it easier to identify where stored 
+{
+    class Program //main class of app
+    {
+        static void Main() //function named Main
+        {
+            Console.WriteLine("THE TECH ACADEMY\n Student Daily Report"); //Console.WriteLine(); code input that will display. Escape character \n starts a new line
+            string heading = Console.ReadLine(); //console.readline(); prevents the console from closing itself so we can view the output of our code input.
+
+            Console.WriteLine("What is your name?\n");
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?\n");
+            string courseName = Console.ReadLine();
+
+            Console.WriteLine("What page number?\n");
+            string pageNumber = Console.ReadLine(); // don't use int. This is not a math operation, just a specific answer
+
+            Console.WriteLine("Do you need help with anything?\n");            
+            string helpQuestion = Console.ReadLine(); //bool could possibly be used for a simple y/n answer. If so, radio buttons or check boxes recommended. String works for space to describe question
+            
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.\n");
+            string yourExperience = Console.ReadLine(); //same notes as above concerning bool
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? please be specific.\n");
+            string otherfeedback = Console.ReadLine(); //same notes as above concerning bool
+
+            Console.WriteLine("How many hours did you study today?\n ");
+            string hoursStudied = Console.ReadLine(); //not a math operation so int is not used. String does the job
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond\n" +
+                "to this shortly. Have a great day!");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ VariablesAndDataTypes/VariablesAndDataTypes.cs/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine(); // cannot use int because program is expecting string. This is not a math operation
+            //Console.WriteLine("Your favorite number is: " + favoriteNumber);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber); //casted string to int so now can be used for math
+            //int Total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + Total);
+            //Console.ReadLine();
+
+
+            //examples of other datatypes
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemp = -2;
+            char questionMark = '\u2103'; //unicode
+
+            //Console.WriteLine(questionMark);
+            //Console.ReadLine();
+
+            decimal moneyInTheBank = 100.5m; // m means decimal
+            double heightInCentimeters = 225.683;
+            float secondsLeft = 13.4f;
+            short temparatureOnMars = -300;
+            string myName = "Miss Jackson if ya nasty";
+
+            int currentAge = 30; // convert int to str
+            string yearsOld = currentAge.ToString();
+
+            bool israining = true;
+            string rainingStatus = Convert.ToString(israining);
+
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+                        
+        }
+    }
+}
