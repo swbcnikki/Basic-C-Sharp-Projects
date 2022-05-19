7ab439a8a973fa52ab6f5232346ffa5765dcc288
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-05-18 23:52:59 -0700

    comments update

--- AnonymousIncomeComparisonProgram/AnonymousIncomeComparisonProgram.cs/Program.cs
+++ AnonymousIncomeComparisonProgram/AnonymousIncomeComparisonProgram.cs/Program.cs
@@ -10,22 +10,25 @@ namespace AnonymousIncomeComparisonProgram.cs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Anonymous Income Comparison Program");
-            Console.ReadLine();
+            Console.WriteLine("Anonymous Income Comparison Program"); //name of program
+            Console.ReadLine();// keeps the console display open so we can see the result
 
+            //identifying the first subject
             Console.WriteLine("Person 1");
             Console.ReadLine();
 
-            Console.WriteLine("Hourly Rate?");
+            //Question in string, hourly rate in integer
+            Console.WriteLine("Hourly Rate?"); 
             Console.ReadLine();
-            int hourlyRate1 = 60;
-            Console.WriteLine(hourlyRate1);
+            int hourlyRate1 = 60; //integer variable and value
+            Console.WriteLine(hourlyRate1); //passing in the variable
             Console.ReadLine();
 
+            //Question in string, hours worked in integer
             Console.WriteLine("Hours worked per week? ");
             Console.ReadLine();
-            int hoursWorked1 = 80;
-            Console.WriteLine(hoursWorked1);
+            int hoursWorked1 = 80; //integer variable and value
+            Console.WriteLine(hoursWorked1); //passing in the variable
             Console.ReadLine();
 
             Console.WriteLine("Person 2");
@@ -47,8 +50,9 @@ namespace AnonymousIncomeComparisonProgram.cs
             Console.WriteLine("Annual Salary of Person 1");
             Console.ReadLine();
 
-            int product1 = 60 * 80 * 52;
-            Console.WriteLine(product1);
+            //multiplying the rate, hours per week and weeks in a year
+            int product1 = 60 * 80 * 52; 
+            Console.WriteLine(product1);//value of the variable after the operation above
             Console.ReadLine();
 
             Console.WriteLine("Annual Salary of Person 2");
@@ -58,9 +62,11 @@ namespace AnonymousIncomeComparisonProgram.cs
             Console.WriteLine(product2);
             Console.ReadLine();
 
+            //comparing the salaries
             Console.WriteLine("Does Person 1 make more money than  Person 2");
             Console.ReadLine();
 
+            //boolean to determine which is greater and to answer the true/false question of who makes more
             bool moMoney = product1 > product2;
             Console.WriteLine(moMoney);
             Console.ReadLine();
