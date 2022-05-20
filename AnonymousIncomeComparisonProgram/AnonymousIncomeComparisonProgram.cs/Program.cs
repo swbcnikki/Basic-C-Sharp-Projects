0029c84365ff686ffe15a73bc4fa2982865be027
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-05-19 19:30:23 -0700

    assignment redo

--- AnonymousIncomeComparisonProgram/AnonymousIncomeComparisonProgram.cs/Program.cs
+++ AnonymousIncomeComparisonProgram/AnonymousIncomeComparisonProgram.cs/Program.cs
@@ -10,66 +10,48 @@ namespace AnonymousIncomeComparisonProgram.cs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Anonymous Income Comparison Program"); //name of program
-            Console.ReadLine();// keeps the console display open so we can see the result
+            Console.WriteLine("Anonymous Income Comparison Program\n"); //name of program            
 
             //identifying the first subject
-            Console.WriteLine("Person 1");
-            Console.ReadLine();
+            Console.WriteLine("Person 1\n");            
 
             //Question in string, hourly rate in integer
-            Console.WriteLine("Hourly Rate?"); 
-            Console.ReadLine();
-            int hourlyRate1 = 60; //integer variable and value
-            Console.WriteLine(hourlyRate1); //passing in the variable
-            Console.ReadLine();
+            Console.WriteLine("What is the hourly Rate?\n");
+            int hourlyRate1 = Convert.ToInt32(Console.ReadLine()); //integer variable and value
 
             //Question in string, hours worked in integer
-            Console.WriteLine("Hours worked per week? ");
-            Console.ReadLine();
-            int hoursWorked1 = 80; //integer variable and value
-            Console.WriteLine(hoursWorked1); //passing in the variable
-            Console.ReadLine();
+            Console.WriteLine("Hours worked per week?\n ");           
+            int hoursWorked1 = Convert.ToInt32(Console.ReadLine()); //integer variable and value
 
+            
             Console.WriteLine("Person 2");
-            Console.ReadLine();
 
-            Console.WriteLine("Hourly Rate?");
-            Console.ReadLine();
-            int hourlyRate2 = 50;
-            Console.WriteLine(hourlyRate2);
-            Console.ReadLine();
+            Console.WriteLine("What is the hourly Rate?\n");
+            int hourlyRate2 = Convert.ToInt32(Console.ReadLine());
+            
 
-            Console.WriteLine("Hours worked per week? ");
-            Console.ReadLine();
-            int hoursWorked2 = 90;
-            Console.WriteLine(hoursWorked2);
-            Console.ReadLine();
+            Console.WriteLine("Hours worked per week? ");           
+            int hoursWorked2 = Convert.ToInt32(Console.ReadLine());
 
-        
-            Console.WriteLine("Annual Salary of Person 1");
-            Console.ReadLine();
+            Console.WriteLine("Annual Salary of Person 1\n");            
 
             //multiplying the rate, hours per week and weeks in a year
-            int product1 = 60 * 80 * 52; 
-            Console.WriteLine(product1);//value of the variable after the operation above
-            Console.ReadLine();
+            int product1 = hourlyRate1 * hoursWorked1 * 52;
+            Console.WriteLine(product1);//value of the variable after the operation above            
 
-            Console.WriteLine("Annual Salary of Person 2");
-            Console.ReadLine();
+            Console.WriteLine("Annual Salary of Person 2\n");
 
-            int product2 = 50 * 90 * 52;
-            Console.WriteLine(product2);
-            Console.ReadLine();
+            int product2 = hourlyRate2 * hoursWorked2 * 52;
+            Console.WriteLine(product2);            
 
             //comparing the salaries
-            Console.WriteLine("Does Person 1 make more money than  Person 2");
-            Console.ReadLine();
-
+            Console.WriteLine("Does Person 1 make more money than  Person 2?\n");
+            
             //boolean to determine which is greater and to answer the true/false question of who makes more
             bool moMoney = product1 > product2;
             Console.WriteLine(moMoney);
-            Console.ReadLine();
+            Console.ReadLine(); // prevents the console window from closing itself
+
         }
     }
 }
--- CarInsuranceApproval/CarInsuranceApproval.cs/Program.cs
+++ CarInsuranceApproval/CarInsuranceApproval.cs/Program.cs
@@ -11,51 +11,29 @@ namespace CarInsuranceApproval.cs
         static void Main(string[] args)
         {
             Console.WriteLine("Car Insurance Qualification"); //title
-            Console.ReadLine(); //prevents console window from closing itself
-
+            
             //minimum age for insurance is 15
             Console.WriteLine("What is your current age?");
-            Console.ReadLine();
+            int currentAge = Convert.ToInt32(Console.ReadLine());
 
-            //setting comparison variables
-            int qualifyingAge = 15;
-            int currentAge = 17;
-
-            //comparison boolean. Is current age greaterthan or equal to qualifying age? True
-            bool ofAge = currentAge >= qualifyingAge;
-            Console.WriteLine(currentAge);
-            Console.ReadLine();
-
+            bool ofAge = currentAge >= 15;
+           
             //maximum for DUI is 0
-            Console.WriteLine("Have you ever had a DUI?");
-            Console.ReadLine();
+            Console.WriteLine("Have many DUIs do you have?");
+            int DUI = Convert.ToInt32(Console.ReadLine());
 
-            //comparison variables
-            int disqualifyingDUI = 1;
-            int yourDUI = 0;
+            bool noDUI = DUI == 0;
 
-            //comparison boolean. Is your number of DUIs 0? True
-            bool DUI = yourDUI >= disqualifyingDUI;
-            Console.WriteLine(DUI);
-            Console.ReadLine();
-
-            // max speeding tickets is 3
+           // max speeding tickets is 3
             Console.WriteLine("How many speeding tickets do you have?");
-            Console.ReadLine();
+            int tickets = Convert.ToInt32(Console.ReadLine());
 
-            //comparison variables
-            int maxTickets = 3;
-            int yourTickets = 2;
-
-            //comparison boolean. Is your number of tickets equal to or less than 3? True
-            bool tickets = maxTickets <= yourTickets;
-            Console.WriteLine(yourTickets);
-            Console.ReadLine();
+            bool maxTickets = tickets <= 3;
 
             //determining boolean logic
-            Console.WriteLine("Do you qualify?");
-            Console.ReadLine();
-            Console.WriteLine(true && true && true);
+            Console.WriteLine("Did you qualify?");
+            bool qualify = ofAge && noDUI && maxTickets;
+            Console.WriteLine(qualify);
             Console.ReadLine();
         }
     }
