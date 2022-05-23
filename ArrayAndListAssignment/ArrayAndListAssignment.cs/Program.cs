e2301987ec262a7bec9a95df0a9e0b004e2566ad
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-05-23 13:25:21 -0700

    initial commit

---
+++ ArrayAndListAssignment/ArrayAndListAssignment.cs/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        //The string array
+        string[] rainbowArray = { "Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet" };
+        //Question for the user
+        Console.WriteLine("Enter a number between 0 and 6 to display a color of the rainbow");
+        //Set up for to enter the index to display the color
+        int color = Convert.ToInt32(Console.ReadLine());
+        if (color <= 6)
+        {
+            Console.WriteLine(rainbowArray[color]); //going into the array and pulling out the color of the index entered by the user
+            Console.ReadLine();// to prevent the screen from closing
+        }
+        else
+        {
+            Console.WriteLine("You have entered a number that is not within this index array.");
+            Console.ReadLine();
+        }
+
+
+
+        //Selecting an index of the int array
+        int[] numArray = { 7, 8, 9 };
+        //Question for the user
+        Console.WriteLine("Input 0 or 1 or 2 to reveal the number at that index");
+        //display the number associated with the index        
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number <= 2) // bool if else
+        {
+            Console.WriteLine(numArray[number]);
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("You have entered a number that is not within this index array.");
+            Console.ReadLine();
+        }
+
+        //list of strings
+        List<string> stateList = new List<string>();
+        stateList.Add("North Carolina");
+        stateList.Add("South Carolina");
+        stateList.Add("North Dakota");
+        stateList.Add("South Dakota");
+        stateList.Add("West Virginia");
+
+        //question to user
+        Console.WriteLine("Select an number between 0 and 4 to reveal a state with a cardinal direction in it's name");
+        int state = Convert.ToInt32(Console.ReadLine()); //int is referring to the index
+
+        //display the string associated with the index entered
+        Console.WriteLine(stateList[state]);
+        Console.ReadLine(); //keep console open to display result
+    }
+}
+
---
+++ ArraysAndLists/ArraysAndLists.cs/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        //creating arrays
+        //first way of initializing an array. Long method
+        int[] numArray = new int[5]; //the 5 means length is set. can never do more than 5 numbers
+        numArray[0] = 5;
+        numArray[1] = 2;
+        numArray[2] = 10;
+        numArray[3] = 200;
+        numArray[4] = 5000;
+
+        Console.WriteLine(numArray[3]);
+        Console.ReadLine();
+
+        //second way of initializing an array. Shorter
+        //no new int number in the [] so the length is not set until initializing is run
+        int[] numArray1 = new int[] { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        Console.WriteLine(numArray1[3]);
+        Console.ReadLine();
+
+        //third way of initializing an array. Even shorter
+        int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        //even though you cannot change the length of the array after initializing, you can change the value of the items within.
+        numArray2[5] = 650;
+        Console.WriteLine(numArray2[5]);
+        Console.ReadLine();
+
+
+        //creating lists
+
+        List<int> intList = new List<int>(); //in lists you do not declare a length
+        intList.Add(4);
+        intList.Add(10);
+        intList.Remove(10);
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+
+
+
+        List<string> stringList = new List<string>();
+        stringList.Add("Hello");
+        stringList.Add("Jesse");
+        stringList.Remove("Jesse");
+
+        Console.WriteLine(stringList[0]);
+        Console.ReadLine();
+    }
+}
+
