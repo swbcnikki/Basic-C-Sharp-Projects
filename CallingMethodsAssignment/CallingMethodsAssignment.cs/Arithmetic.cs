5955df9fd0edcc54f7a6043d2c7cd636f56db130
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-06-01 10:37:58 -0700

    initial commit

---
+++ CallingMethodsAssignment/CallingMethodsAssignment.cs/Arithmetic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment.cs
+{
+    public class Arithmetic
+    {
+        public static int Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+        public static int Subtract(int num1, int num2)
+        {
+            int result = num1 - num2;
+            return result;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            return result;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            int result = num1 / num2;
+            return result;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment.cs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment.cs
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+            int sum = Arithmetic.Add(10, 5);
+            Console.WriteLine("The sum is " + sum);
+            
+            int difference = Arithmetic.Subtract(10, 5);
+            Console.WriteLine("The difference is " + difference);
+            
+            int product = Arithmetic.Multiply (10, 5);
+            Console.WriteLine("The product is " + product);
+            
+            int quotient = Arithmetic.Divide (10, 5);
+            Console.WriteLine("The quotient is " + quotient);
+            Console.ReadLine();
+        }
+               
+    }
+}
