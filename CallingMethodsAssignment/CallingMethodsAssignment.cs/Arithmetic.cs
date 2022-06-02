9e794919c1b026573a31abcf3f7f390cd11e565d
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-06-01 21:41:59 -0700

    correction and comments

--- CallingMethodsAssignment/CallingMethodsAssignment.cs/Arithmetic.cs
+++ CallingMethodsAssignment/CallingMethodsAssignment.cs/Arithmetic.cs
@@ -5,30 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CallingMethodsAssignment.cs
+
+    //Create a class. In that class, create three methods, each of which will take one integer parameter in and
+    //return an integer.The methods should do some math operation on the received parameter.Put this class in a
+    //separate.cs file in the application.
 {
-    public class Arithmetic
+    public class Arithmetic // The class created through the main program. Public so that it is available everywhere
+                            // Solution Explorer > rightclick CallingMethodsAssignment.cs > hover Add > click Class.
+                            // On the popup, select Class > name the class at the bottom of the screen > click Add
     {
-        public static int Add(int num1, int num2)
+        public static int Add(int potato1, int potato2) // creating the first method. Public so its available everywhere,
+                                                        // static coz it's not instatiated, int, name of method, parameters in parenth.
         {
-            int result = num1 + num2;
-            return result;
+            int result = potato1 + potato2; // setting up the operation. Notice the variable names here do not match
+                                            // the ones when the method is called. The can, but they don't have to.
+            return result; // setting what to do with the outcome of the operation. This is a requirement of the method
+                           // for it to work. Return is not the only way.
         }
-        public static int Subtract(int num1, int num2)
+        public static int Subtract(int num1, int num2) // method 2 is subtract
         {
-            int result = num1 - num2;
-            return result;
+            int result = num1 - num2; // operation
+            return result; // what to do with the outcome
         }
-
-        public static int Multiply(int num1, int num2)
+        public static int Multiply(int num1, int num2) // method 3 is multiply
         {
-            int result = num1 * num2;
-            return result;
+            int result = num1 * num2; // operation
+            return result; // what to do with the outcome
         }
-
-        public static int Divide(int num1, int num2)
+        public static int Divide(int num1, int num2) // method 4 is divide
         {
-            int result = num1 / num2;
-            return result;
+            int result = num1 / num2; // opreration
+            return result; // what to do with the outcome
         }
     }
 }
--- CallingMethodsAssignment/CallingMethodsAssignment.cs/Program.cs
+++ CallingMethodsAssignment/CallingMethodsAssignment.cs/Program.cs
@@ -5,24 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CallingMethodsAssignment.cs
+//Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer. The methods should do some
+//math operation on the received parameter. Put this class in a separate .cs file in the application.
+
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) // Main method of the program. We will be calling the methods we created in the class we created.
         {
-           
-            int sum = Arithmetic.Add(10, 5);
-            Console.WriteLine("The sum is " + sum);
+            // The values are not hard coded in. Ask the user to provide the values to be processed
+            Console.WriteLine("To perform the math operation, please enter your first number... "); // value provided is in string and needs to be converted to int
+            int num1 = Convert.ToInt32(Console.ReadLine()); // converting string to int. Variable name can be anything. Ensure variable name for each number is
+                                                            // distinct. It does not need to match the variable names in the called method from Arithmetic class. 
+            Console.WriteLine("Please enter your second number... "); // string value from user
+            int num2 = Convert.ToInt32(Console.ReadLine()); // converting the string value to int
+
+
+
+             // calling created methods add, subtract, multiply and divide from the Arithmetic class we created
+            int sum = Arithmetic.Add(num1, num2); // passing in the variables declared above and performing the operation
+            Console.WriteLine("The sum is " + sum); // displaying the result
+                        
+            int difference = Arithmetic.Subtract(num1, num2); // passing variables
+            Console.WriteLine("The difference is " + difference); // display results
             
-            int difference = Arithmetic.Subtract(10, 5);
-            Console.WriteLine("The difference is " + difference);
+            int product = Arithmetic.Multiply (num1, num2); // passing variables
+            Console.WriteLine("The product is " + product); // display results
             
-            int product = Arithmetic.Multiply (10, 5);
-            Console.WriteLine("The product is " + product);
-            
-            int quotient = Arithmetic.Divide (10, 5);
-            Console.WriteLine("The quotient is " + quotient);
-            Console.ReadLine();
+            int quotient = Arithmetic.Divide (num1, num2); // passing variables
+            Console.WriteLine("The quotient is " + quotient); // display results
+            Console.ReadLine(); // keeping the page open to view the results.
         }
                
     }
