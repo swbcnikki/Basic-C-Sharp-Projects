1093d316da692d6ca15587f83dec4f216d0d32d9
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-06-04 15:37:21 -0700

    initial commit

---
+++ MainMethodAssignment/MainMethodAssignment.cs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment.cs
+
+//Create a class. In that class, create a method that will take in an integer, create a math operation
+//for this integer (addition, subtraction, etc.), then return the answer as an integer.
+{
+    class Program
+    {
+        public static void Main(string[] args)   // public for access everywhere
+        {
+            //instantiate the class and call the first method, passing in an integer. Display the result to the screen.
+
+            Variety intOp = new Variety();    // instance of intOP     
+            int sum = intOp.Plus(3);        // math operation
+            Console.WriteLine(sum);         // Display result
+            
+            //instantiate the class and call the second method, passing in a decimal. Display the result to the screen.
+            Variety decOp = new Variety();      // instance of decOp      
+            int prod = (int)decOp.Square(5.3m); // math operation
+            Console.WriteLine(prod);            // Display result
+            
+
+            //instantiate the class and call the third method, passing in a string that equates to an integer. Display the result to the screen.
+            Variety strOp = new Variety();       // instance of strOp     
+            int prod2 = (int)strOp.Cube("3");   // math operation
+            Console.WriteLine(prod2);           // Display result           
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment.cs/Variety.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment.cs
+
+//Create a class. In that class, create a method that will take in an integer, create a math operation
+//for this integer (addition, subtraction, etc.), then return the answer as an integer.
+{
+   public class Variety
+    {
+        public int Plus(int bubbles) // creating the first method. Public so its available everywhere,
+                                       // there is no static because we are instatiating, data type int, name of method, parameters in parenth.
+        {
+            int result = bubbles + bubbles; // setting up the operation. Notice the variable names here do not match the
+                                      // ones when the method is called. The can, but they don't have to.
+            return result; // setting what to do with the outcome of the operation. This is a requirement of the method
+                           // for it to work. Return is not the only way.
+        }
+
+        public decimal Square(decimal blossom) // datatype decimal, method 2 is square
+        {
+            int result = (int) (blossom * blossom); // operation
+            return result; // what to do with the outcome
+        }
+
+        public int Cube(string buttercup) // datatype string, method 2 is cube
+        {            
+            int buttercup1 = Convert.ToInt32(buttercup);                
+  
+            int result = buttercup1 * buttercup1 * buttercup1; // operation
+            return result; // what to do with the outcome
+        }
+    }
+}
