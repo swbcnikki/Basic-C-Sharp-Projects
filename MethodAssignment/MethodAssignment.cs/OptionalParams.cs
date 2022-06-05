304230c37fa5dc9d5b07a2c5c425745387778877
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-06-04 21:58:19 -0700

    Update OptionalParams.cs

--- MethodAssignment/MethodAssignment.cs/OptionalParams.cs
+++ MethodAssignment/MethodAssignment.cs/OptionalParams.cs
@@ -8,26 +8,28 @@ namespace MethodAssignment.cs
 {
     class OptionalParams
     {
-        public int Addition(int num1)
+        //Create a class. In that class, create a method that takes two integers as parameters. Make one of
+        //them optional. Have the method do a math operation and return an integer result.
+        public int Addition(int num1) // method with 1 param
         {           
-            Addition(num1, null);
-            int result = num1 + num1;
-            return result;
+            Addition(num1, null); // null allows for 1 param to be passed
+            int result = num1 + num1; // math operation with 1 param
+            return result; // result 
 
         }
 
-        public int Addition(int num1, int[] moreNums)
+        public int Addition(int num1, int[] moreNums) // method with 1 required and 1 optional param
         {
-            int result = num1 + num1;
+            int result = num1 + num1; // math operation for the required param
 
-            if (moreNums != null)
+            if (moreNums != null) // if statement for optional param
             {
-                foreach (int i in moreNums)
+                foreach (int i in moreNums) // for each additional optional param...
                 {
-                    result += i;
+                    result += i; // ...add to the original result
                 }
             }
-            return result;
+            return result; // return the end result
         }
            
         
