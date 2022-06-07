8e483888dcdb2614bdcff88a29350051d153a339
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-06-07 05:56:24 -0700

    initial commit

---
+++ MethodClassAssignment/MethodClassAssignment.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment.cs
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Void binary = new Void(); // instance
+
+            
+            binary.Brain(7, 6);
+
+            binary.Brain(num2: 7, num1: 6);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment.cs/Void.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment.cs
+{
+    //Create a class. In that class, create a void method that takes two integers as parameters. Have the method do
+    //a math operation on the first integer and display the second integer to the screen.
+    public class Void
+    {
+        public void Brain(int num1, int num2)
+        {
+            int work = num1 * num1;
+
+            Console.WriteLine(num2);            
+                        
+        }
+    }
+}
