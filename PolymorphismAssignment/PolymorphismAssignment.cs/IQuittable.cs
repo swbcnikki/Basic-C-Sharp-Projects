eb8104ede145a1624fee41a55bb4455bda8632b2
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-06-08 13:27:15 -0700

    initial commit with questions

---
+++ PolymorphismAssignment/PolymorphismAssignment.cs/IQuittable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment.cs
+{
+    //Create an interface called IQuittable and have it define a void method called Quit().
+    interface IQuittable
+    {
+        void Quit();       
+    }
+}
