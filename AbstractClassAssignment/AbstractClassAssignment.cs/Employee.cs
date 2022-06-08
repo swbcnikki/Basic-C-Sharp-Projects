5e4cc8a7ab3c4f7a6ccf1b35b128333a60dfa1de
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-06-08 08:34:06 -0700

    initial commit

---
+++ AbstractClassAssignment/AbstractClassAssignment.cs/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment.cs
+{
+    public class Employee : Person
+    {
+        public override void Name()
+        {
+            throw new NotImplementedException();
+        }
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ AbstractClassAssignment/AbstractClassAssignment.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.FirstName = "Sample"; // initializing the first name
+            employee.LastName = "Student"; // initializing the last name
+            employee.SayName(); // calling method
+
+            Console.ReadLine();
+        }
+    }
+}
