d830f43fb7c39e2542c7ad327a4141f2d63413d5
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-06-14 01:04:58 -0700

    initial commit

---
+++ ConstructorAssignment/ConstructorAssignment.cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment.cs
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Qualifications newAbility = new Qualifications("Ruthlessness"); // testing the new chain constructor - I'm not entirely sure how this is supposed to test it but we will talk about it tomorrow
+
+            const string squadName = "The Deadly Viper Assassination Squad";
+
+            Console.WriteLine("Meet the {0}. \n " +
+                "Beatrix 'Black Mamba', \n Vernita 'Copperhead', \n O-Ren 'Cottonmouth', \n " +
+                "Elle 'California Mountain Snake', \n Budd 'Sidewinder' \n and their leader Bill 'Snake Charmer'.", squadName);
+            
+            Console.ReadLine();
+
+        }
+        
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment.cs/Qualifications.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment.cs
+{
+    //Chaining two constructors together
+    public class Qualifications
+    {
+        public Qualifications(string availability) : this(availability, "yes")
+        {
+        }
+        public Qualifications(string availability, string capability)
+        {
+            List<string> DeathList = new List<string>();
+            var When = availability; // creating variable using var
+            var Will = capability; // creating variable using var
+        }
+    }
+}
+
+
+/*Perform these actions and create a console app that includes the following:
+
+
+Create a const variable.
+
+Create a variable using the keyword “var.”
+
+Chain two constructors together.
+
+Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code. */
