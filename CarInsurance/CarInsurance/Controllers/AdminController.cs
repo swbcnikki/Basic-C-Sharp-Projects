81df80ac9e7e280c52c6b96cd0a81acca791e180
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-06-22 02:10:28 -0700

    initial commit

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using CarInsurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        private InsuranceEntities db = new InsuranceEntities();
+        public ActionResult Index()
+        {
+            return View(db.Insurees.ToList());
+        }
+    }
+}
