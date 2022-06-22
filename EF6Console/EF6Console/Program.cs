c233d038dfb20a67ae10f9382c689dea87d40d6a
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-06-22 14:41:12 -0700

    initial commit

---
+++ EF6Console/EF6Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EF6Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Grumpy" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+                Console.WriteLine("Student saved successfully!");
+                
+                Console.ReadLine();
+
+
+            }
+            
+        }
+                
+    }
+}
---
+++ EF6Console/EF6Console/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EF6Console.Program;
+
+namespace EF6Console
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
