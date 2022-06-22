5bec3df15139a621904f39ae79e3f05697d5a1ef
swbcnikki <99786405+swbcnikki@users.noreply.github.com>
2022-06-21 23:41:51 -0700

    commit

---
+++ InputAssignment/InputAssignment.cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+
+
+/* Perform these actions and create a console app that includes the following:
+
+
+Asks the user for a number.
+
+Logs that number to a text file.
+
+Prints the text file back to the user.
+
+Add comments to each line or block of your code to explain what it does exactly, so that another developer could read and understand your code.
+
+Upload your code to GitHub. */
